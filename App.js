32d4186b1ba2a14dd957a94454eb99a3460977a2
esperor <pere_strelka@mail.ru>
2024-05-25 12:53:25 +0500

    initial

---
+++ App.js
@@ -0,0 +1,17 @@
+import { render } from "preact";
+import { useReducer } from "preact/hooks";
+import { html } from "https://esm.sh/htm@3.1.1/preact?external=preact";
+
+function App() {
+  const [count, add] = useReducer((a, b) => a + b, 0);
+
+  return html`
+    <button onClick=${() => add(-1)}>Decrement</button>
+    <input readonly size="4" value=${count} />
+    <button onClick=${() => add(1)}>Increment</button>
+  `;
+}
+
+render(html`<${App} />`, document.body);
+
+export default App;
