0fe3f2eda08e9adbaac02c52945b3f128ebae077
esperor <pere_strelka@mail.ru>
2024-05-25 18:00:42 +0500

    feat: implement session

--- src/components/menu/button.js
+++ src/components/menu/button.js
@@ -1,6 +1,4 @@
 import { html } from "https://esm.sh/htm@3.1.1/preact?external=preact";
-import "./button.css";
-
 /**
  *
  * @param {{data: Document, onClick: (e, id) => void}}
@@ -24,5 +22,47 @@ function Button({ data, onClick }) {
     </button>
   `;
 }
+
+document.head.insertAdjacentHTML(
+  "beforeend",
+  `<style>
+.menu__button {
+  display: flex;
+  flex-direction: row;
+  gap: 0.75rem;
+  border-radius: 0.5rem;
+  padding: 0.75rem 1rem;
+  background-color: #d2d2d2;
+  width: 90%;
+  margin-left: auto;
+  margin-right: auto;
+  box-shadow: 0 0 0.3rem 0.05rem #777;
+  scale: 1;
+  transition: scale 0.2s ease-in-out;
+  cursor: pointer;
+  border: 0;
+  align-items: center;
+  &:hover {
+    scale: 1.025;
+  }
+  &:active {
+    transition: scale 0.1s ease-in-out;
+    scale: 0.975;
+  }
+}
+
+.menu__button__icon {
+  width: 1.5rem;
+  height: 1.5rem;
+}
+
+.menu__button__label {
+  font-size: 1rem;
+  font-family: Verdana, Geneva, Tahoma, sans-serif;
+  color: #333;
+  cursor: pointer;
+}
+</style>`
+);
 
 export default Button;
